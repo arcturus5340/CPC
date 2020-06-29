be3fd4045dd6a0a3ddcf5005a21daae0a6cad795
arcturus5340 <arcturus5340@gmail.com>
2020-06-29 19:51:50 +0200

    refuse of random moves on the server side
    Let's take a step towards api!

--- chess_app/apps.py
+++ chess_app/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class ChessAppConfig(AppConfig):
     name = 'chess_app'
-
-    def ready(self):
-        from chess_app import boardUpdater
-        boardUpdater.start()
